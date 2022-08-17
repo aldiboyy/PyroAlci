4d364f5ac6df4113537f2522b2be418debd4343a
aldiboyy <108301147+aldiboyy@users.noreply.github.com>
2022-08-17 23:09:11 +0700

    Update __main__.py

--- WhyzuProject/__main__.py
+++ WhyzuProject/__main__.py
@@ -18,7 +18,7 @@ from WhyzuProject.helpers.misc import heroku
 from WhyzuProject.modules import ALL_MODULES
 
 MSG_ON = """
-🔥 **Whyzu-Userbot Berhasil Di Aktifkan**
+🔥 **Alci-Userbot Berhasil Di Aktifkan**
 ━━
 ➠ **Userbot Version -** `{}`
 ➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
@@ -33,8 +33,8 @@ async def main():
         try:
             await bot.start()
             bot.me = await bot.get_me()
-            await bot.join_chat("WhyzuCH")
-            await bot.join_chat("WhyzuRoom")
+            await bot.join_chat("soasiklo69")
+            await bot.join_chat("alxelsupport")
             await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
             LOGGER("WhyzuProject").info(
                 f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
